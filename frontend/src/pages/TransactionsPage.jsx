046767128915a83f48ecb9083ba8d9623b9eca01
Tu Nombre <tu@email.com>
2026-01-25 10:10:28 -0300

    fix: Syntax error in TransactionsPage.jsx

--- frontend/src/pages/TransactionsPage.jsx
+++ frontend/src/pages/TransactionsPage.jsx
@@ -117,7 +117,6 @@ export default function TransactionsPage() {
                     </div>
                 </div>
             </div>
-        </div>
-        </AdminLayout >
+        </AdminLayout>
     )
 }
